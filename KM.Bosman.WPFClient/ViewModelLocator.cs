99d91663ac765db385f65e37fbc074a9f6d3398c
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2019-02-13 15:51:57 +0100

    day 1

--- KM.Bosman.WPFClient/ViewModelLocator.cs
+++ KM.Bosman.WPFClient/ViewModelLocator.cs
@@ -1,4 +1,7 @@
-﻿using KM.Bosman.FakeServices;
+﻿using Autofac;
+using Autofac.Extras.CommonServiceLocator;
+using CommonServiceLocator;
+using KM.Bosman.FakeServices;
 using KM.Bosman.IServices;
 using KM.Bosman.ViewModels;
 using System;
@@ -14,23 +17,49 @@ namespace KM.Bosman.WPFClient
     {
         // public SpeedsViewModel SpeedsViewModel => new SpeedsViewModel(new FakeSpeedService());
 
-        private IUnityContainer container;
+        // private IUnityContainer container;
+        // private IContainer container;
 
         public ViewModelLocator()
         {
-            UseUnity();
+            // UseUnity();
+
+            UseAutoFac();
         }
 
 
         // Install-Package Unity
         private void UseUnity()
         {
-            container = new UnityContainer();
-            container.RegisterType<ISpeedService, FakeSpeedService>();
+            IUnityContainer container = new UnityContainer();
+            //          container.RegisterType<ISpeedService, FakeSpeedService>();
+            container.RegisterSingleton<ISpeedService, FakeSpeedService>();
             container.RegisterType<SpeedsViewModel>();
+
+        //  ServiceLocator.SetLocatorProvider(() => new UnityServiceLocator(container));
         }
 
-        public SpeedsViewModel SpeedsViewModel => container.Resolve<SpeedsViewModel>();
+        //  public SpeedsViewModel SpeedsViewModel => container.Resolve<SpeedsViewModel>();
+
+
+        public SpeedsViewModel SpeedsViewModel => ServiceLocator.Current.GetInstance<SpeedsViewModel>();
+
+        // Install-Package AutoFac
+        private void UseAutoFac()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<FakeSpeedService>().As<ISpeedService>().SingleInstance();
+            builder.RegisterType<SpeedsViewModel>();
+            builder.RegisterType<SpeedFaker>();
+
+            IContainer container = builder.Build();
+
+            // Install-Package Autofac.Extras.CommonServiceLocator
+            ServiceLocator.SetLocatorProvider(() => new AutofacServiceLocator(container));
+
+        }
+
+        // Install-Package CommonServiceLocator
 
 
 
